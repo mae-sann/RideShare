95bbf4629a395b634cc298adf4ed1b261ce677f4
Charry Mae Atamosa <charrymaeatamosa@gmail.com>
2025-11-18 02:00:42 +0800

    Updated User Profile with backend

--- ride_share/profile_app/forms.py
+++ ride_share/profile_app/forms.py
@@ -1,50 +1,19 @@
 from django import forms
 from django.contrib.auth.forms import PasswordChangeForm
-from django.contrib.auth.password_validation import validate_password
-from django.core.exceptions import ValidationError
-from .models import Profile
-import re
 
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['phone']
-        widgets = {
-            'phone': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'e.g., 09123456789'
-            }),
-        }
-        labels = {
-            'phone': 'Phone Number',
-        }
-    
-    def clean_phone(self):
-        phone = self.cleaned_data.get('phone')
-        if phone and not phone.isdigit():
-            raise forms.ValidationError('Phone number should contain only digits.')
-        if phone and len(phone) < 10:
-            raise forms.ValidationError('Phone number should be at least 10 digits long.')
-        return phone
-
-class ProfilePictureForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['profile_picture_url']
-        widgets = {
-            'profile_picture_url': forms.FileInput(attrs={
-                'class': 'hidden',
-                'id': 'id_profile_picture',
-                'accept': 'image/jpeg,image/png'
-            })
-        }
-        labels = {
-            'profile_picture_url': 'Profile Picture'
-        }
+class ProfileForm(forms.Form):
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=30, required=False)
+    username = forms.CharField(max_length=150, required=True)
+    phone = forms.CharField(max_length=20, required=False)
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['profile_picture_url'].required = False
+        # Set up form fields with classes
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
 
 class CustomPasswordChangeForm(PasswordChangeForm):
     def __init__(self, *args, **kwargs):
